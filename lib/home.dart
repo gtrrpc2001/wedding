bff9892a1f1d4b95e265bba9b227fb1265d66347
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-07-27 15:40:06 +0900

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -21,6 +21,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           children: [
             Welcome(),
+            CustomDivider(),
             Introduction(),
             CustomDivider(),
             Gallery(),
