7477d8700e9af3392d3dc8f72e24d8a99aca1755
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-07-27 18:11:28 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'home.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
