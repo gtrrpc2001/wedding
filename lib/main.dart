6520c9c2dba992eb68713deb28d956fee487f996
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-07-28 12:10:21 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   //WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
-  await dotenv.load(fileName: ".env");  
+  //await dotenv.load(fileName: ".env");  
 }
 
 class MyApp extends StatelessWidget {
