64cf3c328d3d8515d474fa6a063c47852421f1e6
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-07-28 16:35:06 +0900

    Update congratulation_gift.dart

--- lib/widgets/congratulation_gift.dart
+++ lib/widgets/congratulation_gift.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:get/get.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 
 class CongratulationGift extends StatelessWidget {
